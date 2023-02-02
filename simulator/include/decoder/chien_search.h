899d6da3afdb9ab8761513ab6620978115c08f41
PleYa <aaiaia.gg@gmail.com>
2023-02-02 16:54:28 +0900

    detached chien search functions

---
+++ simulator/include/decoder/chien_search.h
@@ -0,0 +1,51 @@
+enum CHIEN_SEARCH_DIRECTION
+{
+    CHIEN_SEARCH_DIRECTION_BACKWARD = 0,
+    CHIEN_SEARCH_DIRECTION_FORWARD = 1,
+}typedef enum_CHIEN_SEARCH_DIRECTION;
+
+char calculateChienSearchVariableSet
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldPolyForm **variableSet,
+    struct_galoisFieldPolyForm *firstOrder,
+    unsigned int orderLength
+);
+char calculateChienSearch_direction
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *errLocPolynomial,
+    struct_powerFormPolynomials *errorCorectableCodeWord,
+    unsigned int *metricCheck,
+    enum CHIEN_SEARCH_DIRECTION direction
+);
+char chienSearch_offset_direction
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *errLocPolynomial,
+    struct_powerFormPolynomials *errorLocationPolynomial,
+    unsigned int *metricCheck,
+    enum CHIEN_SEARCH_DIRECTION direction
+);
+char calculateChienSearch_static_backWard
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *errLocPolynomial,
+    struct_powerFormPolynomials *errorCorectableCodeWord,
+    unsigned int *metricCheck
+);
+char chienSearch_static
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *errLocPolynomial,
+    struct_powerFormPolynomials *errorLocationPolynomial,
+    unsigned int *metricCheck
+);
+char chienSearch
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *errLocPolynomial,
+    struct_powerFormPolynomials *errorLocationPolynomial,
+    unsigned int *metricCheck,
+    enum KIND_OF_BCH_DECODING_ALGORITHM KIND_OF_BCH_ALGORITHM
+);
