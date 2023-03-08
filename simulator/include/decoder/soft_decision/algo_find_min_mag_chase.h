8bc175b2d563b1116ab0d716a96de36a4a1f7ee8
PleYa <aaiaia.gg@gmail.com>
2023-03-09 00:07:04 +0900

    detached functions are about finding minimum llr magnitude related with chase algorithm

--- simulator/include/decoder/soft_decision/algo_find_min_mag_chase.h
+++ simulator/include/decoder/soft_decision/algo_find_min_mag_chase.h
@@ -1,3 +1,9 @@
 #define DIRECTION_LEFT    0
 #define DIRECTION_RIGHT    1
 
+char findMinimumMagnitudeLogLikeHoodRatio_chaseAlgorithm_followMinPath(struct_logLikeHoodRatio *p);
+char sortMinimumMagnitudeLogLikeHoodRatio_chaseAlgorithm(
+    struct_logLikeHoodRatio *p,
+    struct_powerFormPolynomials *hd_codeword,
+    unsigned int zero_magnitude_sel_policy,
+    unsigned int same_magnitude_sel_policy);
