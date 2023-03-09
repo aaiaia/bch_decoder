6c172463427f4d013ae54e06feda2cc25c903561
PleYa <aaiaia.gg@gmail.com>
2023-03-09 12:06:36 +0900

    detached function passingThroughAwgnData()

---
+++ simulator/include/file/awgn.h
@@ -0,0 +1,23 @@
+/* Example codes in main functions
+//while((loadStream.countedDataNumber<loadStream.totalDataNumber))
+//{
+//    printf("[%0000006d] : ", loadStream.countedCodeWordNumber);
+//    loadToCodeWordStartAtHighSide(main_encodingComponentInGF->codeWord, &loadStream);
+//    unloadFromCodeWordStartAtHighSide(&unloadStream, main_encodingComponentInGF->codeWord);
+//    printPowerFormWithEnterPolynomialWithTapUsingAddress(main_encodingComponentInGF->codeWord);
+//    printf("[S:%d, U:%d, M:%d]\n",strlen(main_encodingComponentInGF->codeWord->equation), main_encodingComponentInGF->codeWord->usedLength, main_encodingComponentInGF->codeWord->length);
+//}
+//printBmpFileToHexCode2();
+//saveBmpFile2(0);
+*/
+char passingThroughAwgnData(
+    double filePass_EbN0,
+    unsigned int filePass_parityBitsLength,
+    char *filePass_generationPolynomial,
+    unsigned int filePass_selectedNumberOfSetOfConjugacy,
+    unsigned int filePass_codeLength,
+    struct_galoisField_info *filePass_galoisField,
+    struct_variableSetConvertBitStreamToPowerForm *loadStream,
+    struct_variableSetConvertBitStreamToPowerForm *unloadStream,
+    struct_variableSetConvertBitStreamToPowerForm *noisedStream
+);
