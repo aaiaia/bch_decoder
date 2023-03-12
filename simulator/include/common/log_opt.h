30dac3e4a48b1808cd4ae917efc6e7a3e2bb35cc
PleYa <aaiaia.gg@gmail.com>
2023-03-13 04:16:40 +0900

    minar marked that not used defines

--- simulator/include/common/log_opt.h
+++ simulator/include/common/log_opt.h
@@ -42,11 +42,11 @@ unsigned int global_flag_cmdSystemUnhold = 0;
     /* Algorithm debugging options start */
     /* BM Algorithm debug option start */
     unsigned int global_flag_debug_bmSim = 0;
-    #define DEBUG_BCH_BM_ALGORITHM_WORD         "debug-bm"//OPT_DISPLAY_PROGRESS
+    #define DEBUG_BCH_BM_ALGORITHM_WORD         "debug-bm"  //not used
     #define FLAG_MASK_DEBUG_BCH_BM_ALGORITHM    (1<<0)
     #define debugBmAlgorithmMes                 printf("debug-BM ) ");
 
-    #define DEBUG_BCH_BM_CAL_DELTA_WORD         "debug-bm-cal-delta"//OPT_DISPLAY_PROGRESS
+    #define DEBUG_BCH_BM_CAL_DELTA_WORD         "debug-bm-cal-delta"    //not used
     #define FLAG_MASK_DEBUG_BCH_BM_CAL_DELTA    (1<<1)
     #define debugBmAlgorithmCalDeltaMes         printf("debug-BM-cal-delta ) ");
     /* BM Algorithm debug option end */
@@ -54,11 +54,11 @@ unsigned int global_flag_cmdSystemUnhold = 0;
     /*mSBS Algorithm debug option start*/
     /* below variables could be unnecessary */
     unsigned int global_flag_debug_mSBS_Sim = 0;
-    #define DEBUG_BCH_mSBS_ALGORITHM_WORD       "debug-mSBS"//OPT_DISPLAY_PROGRESS
+    #define DEBUG_BCH_mSBS_ALGORITHM_WORD       "debug-mSBS"    //not used
     #define FLAG_MASK_DEBUG_BCH_mSBS_ALGORITHM  (1<<0)
     #define debug_mSBS_AlgorithmMes             printf("debug-mSBS ) ");
 
-    #define DEBUG_BCH_M_SBS_CAL_DELTA_WORD      "debug-mSBS-cal-delta"//OPT_DISPLAY_PROGRESS
+    #define DEBUG_BCH_M_SBS_CAL_DELTA_WORD      "debug-mSBS-cal-delta"  //not used
     #define FLAG_MASK_DEBUG_BCH_M_SBS_CAL_DELTA (1<<1)
     #define debug_mSBS_AlgorithmCalDeltaMes     printf("debug-mSBS-cal-delta ) ");
     /*mSBS Algorithm debug option end*/
