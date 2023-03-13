09a4e130e961c798287ce77f6fb84066bd60077a
PleYa <aaiaia.gg@gmail.com>
2023-03-14 00:04:11 +0900

    minar added linefeed(enter)

--- simulator/include/common/cmd_opt_vars.h
+++ simulator/include/common/cmd_opt_vars.h
@@ -500,8 +500,10 @@ unsigned int global_bch_SoftCorrectable = DEFAULT_VALUE_BCH_SOFT_CORRECTABLE;
 #define DEFAULT_VALUE_BCH_SOFT_OVER_SYND    0
 unsigned int global_bch_Soft_OverSyndLen = DEFAULT_VALUE_BCH_SOFT_OVER_SYND;
 /* flags for generating galois field(GF) end */
+
 /* non-categorized flags start */
 char *global_AddedRoot = NULL;  //notused
 /* non-categorized flags end */
+
 /* Operation */
 void GLOBAL_flag_proc();
