a3ba81d1a47976e049853c07ab1e4231a04fc0b0
PleYa <aaiaia.gg@gmail.com>
2023-03-14 02:31:49 +0900

    detached functions about Least Common Multiple(LCM)

--- simulator/include/gf/equation.h
+++ simulator/include/gf/equation.h
@@ -14,4 +14,22 @@ struct_galoisFieldElements* multipleTwoEqReturnAddr(
 );
 
 /* etc */
-unsigned int checkDegreePolynomials(struct_galoisFieldElements *galoisFields, struct_galoisFieldElements *targetPolynomial)
+unsigned int checkDegreePolynomials(struct_galoisFieldElements *galoisFields, struct_galoisFieldElements *targetPolynomial);
+
+/* Least Common Multiple(LCM) expantion */
+char abstractOrderOfEquation(struct_galoisFieldElements *equation);
+struct_galoisFieldElements *expandRootOfLCM_usingEleOfGF
+(
+
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *equation,
+    struct_setOfGaloisFieldElements *multipledRootSet,
+    unsigned int numberOfSelectedSet//numberOfSelectedSet is same that number of correctable bit
+);
+struct_galoisFieldElements *test_expandLimitedLeastCommonMultipleUsingElementOfGaloisFieldToRoot
+(
+    struct_galoisFieldElements *galoisFields,
+    struct_galoisFieldElements *equation,
+    struct_setOfGaloisFieldElements *multipledRootSet,
+    unsigned int numberOfSelectedSet//numberOfSelectedSet is same that number of correctable bit
+);
