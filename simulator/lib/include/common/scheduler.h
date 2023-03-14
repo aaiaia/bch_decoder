272ec127eb7519bc74018834c67f82b334b858a3
PleYa <aaiaia.gg@gmail.com>
2023-03-14 10:19:08 +0900

    fixed typo @ common/scheduler.h

--- simulator/lib/include/common/scheduler.h
+++ simulator/lib/include/common/scheduler.h
@@ -6,6 +6,6 @@ struct struct_scheduling_FIFO
     enum_list_valueType valueType;
     void *list;
     uint32_t length;
-}typedef struct_scheduling_FIFO;a
+}typedef struct_scheduling_FIFO;
 
 #endif
