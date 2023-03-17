64a2613659dc89a48ce9c3ccb5ac431a5f8a5fe4
PleYa <aaiaia.gg@gmail.com>
2023-03-17 15:42:06 +0900

    added property extern to make global @ func_verify.h

--- simulator/lib/include/common/func_verify.h
+++ simulator/lib/include/common/func_verify.h
@@ -13,31 +13,31 @@ enum ENUM_FLAG_CASE_SIM_TEST_OPTIONS
     FLAG_CASE_SIM_TEST_MODE_SHOW_LOOP_CNT,
 };
 
-unsigned int global_flag_case_sim_testOpt = 0;
+extern unsigned int global_flag_case_sim_testOpt;
 
 /* Test Mode Commonly using variables */
 /* Test mode iteration variables */
 #define TMP_TEST_MODE_LOOPS             1000000UL
 #define TMP_TEST_MODE_ZERO_BIT_RATIO    0.5f
 
-unsigned long tmp_testMode_loops = TMP_TEST_MODE_LOOPS;
+extern unsigned long tmp_testMode_loops;
 
 
 /* Error Distribution */
-FILE *fp_testMode_log = NULL;
-FILE *fp_testMode_sum_bit_cnt_arranged_by_bitRetio_csv= NULL;
-FILE *fp_testMode_sum_bit_cnt_classified_arranged_by_bitRatio_csv = NULL;
-FILE *fp_testMode_sum_llr_mag_arranged_by_bitRatio_csv = NULL;
-FILE *fp_testMode_sum_err_bit_cnt_chg_classified_arranged_by_bitRatio_csv = NULL;
+extern FILE *fp_testMode_log;
+extern FILE *fp_testMode_sum_bit_cnt_arranged_by_bitRetio_csv;
+extern FILE *fp_testMode_sum_bit_cnt_classified_arranged_by_bitRatio_csv;
+extern FILE *fp_testMode_sum_llr_mag_arranged_by_bitRatio_csv;
+extern FILE *fp_testMode_sum_err_bit_cnt_chg_classified_arranged_by_bitRatio_csv;
 
-FILE *fp_testMode_all_arranged_by_bitRatio_csv = NULL;
+extern FILE *fp_testMode_all_arranged_by_bitRatio_csv;
 
 
-FILE *fp_testMode_sum_codeword_cnt_arranged_by_errorNums_csv = NULL;
-FILE *fp_testMode_sum_llr_mag_distribution_cnt_arranged_by_errorNums_csv = NULL;
-FILE *fp_testMode_sum_bit_ratio_cnt_arranged_by_errorNums_csv = NULL;
+extern FILE *fp_testMode_sum_codeword_cnt_arranged_by_errorNums_csv;
+extern FILE *fp_testMode_sum_llr_mag_distribution_cnt_arranged_by_errorNums_csv;
+extern FILE *fp_testMode_sum_bit_ratio_cnt_arranged_by_errorNums_csv;
 
-FILE *fp_testMode_all_arranged_by_errorNums_csv = NULL;
+extern FILE *fp_testMode_all_arranged_by_errorNums_csv;
 
 enum ENUM_CASE_TESTMODE_ERR_RATE_MSG_GEN_METH
 {
@@ -45,8 +45,7 @@ enum ENUM_CASE_TESTMODE_ERR_RATE_MSG_GEN_METH
     ENUM_CASE_TESTMODE_ERR_RATE_MSG_GEN_METH_STATIC,
     ENUM_CASE_TESTMODE_ERR_RATE_MSG_GEN_METH_RAND,
 };
-unsigned int global_flag_case_testMode_msg_gen_method =
-                ENUM_CASE_TESTMODE_ERR_RATE_MSG_GEN_METH_STATIC;
+extern unsigned int global_flag_case_testMode_msg_gen_method;
 
 enum ENUM_CASE_TESTMODE_ERR_RATE_DECODING_METH
 {
@@ -54,32 +53,31 @@ enum ENUM_CASE_TESTMODE_ERR_RATE_DECODING_METH
     ENUM_CASE_TEST_MODE_ERR_RATE_DECODING_METH_HD,
     ENUM_CASE_TEST_MODE_ERR_RATE_DECODING_METH_SD,
 };
-unsigned int global_flag_case_testMode_decoding_method =
-                ENUM_CASE_TEST_MODE_ERR_RATE_DECODING_METH_HD;
+extern unsigned int global_flag_case_testMode_decoding_method;
 
 /* encoding parts arranged by bit ratio */
-unsigned int tmp_testMode_zero_bit_nums_inC = 0;
-unsigned int tmp_testMode_one_bit_nums_inC = 0;
+extern unsigned int tmp_testMode_zero_bit_nums_inC;
+extern unsigned int tmp_testMode_one_bit_nums_inC;
 
 /*
  * account of tmp_bit_num_inC_ref_zero_OZ_ratio
  * index 0 -> zero bit num in Codeword.
  * index 1 -> one bit num in Codeword.
  */
-unsigned long** tmp_bit_num_inC_ref_zero_OZ_ratio = NULL;
+extern unsigned long** tmp_bit_num_inC_ref_zero_OZ_ratio;
 
 /* decoding parts arranged by bit ratio */
-unsigned int tmp_testMode_zeroToOne_bit_nums_inR = 0;//received bit change detect
-unsigned int tmp_testMode_oneToZero_bit_nums_inR = 0;//received bit change detect
+extern unsigned int tmp_testMode_zeroToOne_bit_nums_inR;//received bit change detect
+extern unsigned int tmp_testMode_oneToZero_bit_nums_inR;//received bit change detect
 
-unsigned int tmp_testMode_zero_is_inR_w = 0;//received bits cnt, which are zero or one and is error
-unsigned int tmp_testMode_one_is_inR_w = 0;//received bits cnt, which are zero or one and is error
+extern unsigned int tmp_testMode_zero_is_inR_w;//received bits cnt, which are zero or one and is error
+extern unsigned int tmp_testMode_one_is_inR_w;//received bits cnt, which are zero or one and is error
 
-unsigned int tmp_testMode_zero_is_inR_c = 0;//received bits cnt, which are zero or one and is correct
-unsigned int tmp_testMode_one_is_inR_c = 0;//received bits cnt, which are zero or one and is correct
+extern unsigned int tmp_testMode_zero_is_inR_c;//received bits cnt, which are zero or one and is correct
+extern unsigned int tmp_testMode_one_is_inR_c;//received bits cnt, which are zero or one and is correct
 
-unsigned long tmp_testMode_llr_mag_sum_inR_w =0UL;//llr sum in received codeword wrong case
-unsigned long tmp_testMode_llr_mag_sum_inR_c =0UL;//llr sum in received codeword good case
+extern unsigned long tmp_testMode_llr_mag_sum_inR_w;//llr sum in received codeword wrong case
+extern unsigned long tmp_testMode_llr_mag_sum_inR_c;//llr sum in received codeword good case
 
 
 /*
@@ -88,7 +86,7 @@ unsigned long tmp_testMode_llr_mag_sum_inR_c =0UL;//llr sum in received codeword
  * index 1 -> zero bit nums in correct bit
  * index 2 -> zero bit nums in error bit
  */
-unsigned long** tmp_bit_num_inR_ref_zero_OZ_ratio = NULL;
+extern unsigned long** tmp_bit_num_inR_ref_zero_OZ_ratio;
 
 /*
  * account of tmp_bit_num_inR_ref_one_OZ_ratio
@@ -96,7 +94,7 @@ unsigned long** tmp_bit_num_inR_ref_zero_OZ_ratio = NULL;
  * index 1 -> one bit nums in correct bit
  * index 2 -> one bit nums in error bit
  */
-unsigned long** tmp_bit_num_inR_ref_one_OZ_ratio = NULL;
+extern unsigned long** tmp_bit_num_inR_ref_one_OZ_ratio;
 
 /*
  * account of tmp_bit_chg_cnt_inR_OZ_ratio
@@ -104,7 +102,7 @@ unsigned long** tmp_bit_num_inR_ref_one_OZ_ratio = NULL;
  * index 1 -> nums of error bit transacting 0 to 1.
  * index 2 -> nums of error bit transacting 1 to 0.
  */
-unsigned long** tmp_bit_chg_cnt_inR_OZ_ratio = NULL;
+extern unsigned long** tmp_bit_chg_cnt_inR_OZ_ratio;
 
 /*
  * account of tmp_llr_mag_sum_inR_OZ_ratio
@@ -112,75 +110,73 @@ unsigned long** tmp_bit_chg_cnt_inR_OZ_ratio = NULL;
  * index 1 -> summation of llr magnitude at corrected bit.
  * index 2 -> summation of llr magnitude at error bit.
  */
-unsigned long** tmp_llr_mag_sum_inR_OZ_ratio=NULL;
+extern unsigned long** tmp_llr_mag_sum_inR_OZ_ratio;
 
-double** tmp_llr_mag_inR = NULL;
+extern double** tmp_llr_mag_inR;
 
 
 /* data arranged by number of error */
-unsigned long* tmp_sum_err_codeword_cnt_arranged_by_err_nums = NULL;//after chk codeword
-unsigned long** tmp_err_llr_mag_distribution_in_all_loop_arranged_by_err_nums = NULL;//after chk codeword
-unsigned int* tmp_err_llr_mag_distribution_in_one_codeword = NULL;//in codeword chking
+extern unsigned long* tmp_sum_err_codeword_cnt_arranged_by_err_nums;//after chk codeword
+extern unsigned long** tmp_err_llr_mag_distribution_in_all_loop_arranged_by_err_nums;//after chk codeword
+extern unsigned int* tmp_err_llr_mag_distribution_in_one_codeword;//in codeword chking
 
-unsigned long* tmp_sum_cor_codeword_cnt_arranged_by_err_nums = NULL;//after chk codeword
-unsigned long** tmp_cor_llr_mag_distribution_in_all_loop_arranged_by_err_nums = NULL;//after chk codeword
-unsigned int* tmp_cor_llr_mag_distribution_in_one_codeword = NULL;//in codeword chking
+extern unsigned long* tmp_sum_cor_codeword_cnt_arranged_by_err_nums;//after chk codeword
+extern unsigned long** tmp_cor_llr_mag_distribution_in_all_loop_arranged_by_err_nums;//after chk codeword
+extern unsigned int* tmp_cor_llr_mag_distribution_in_one_codeword;//in codeword chking
 
 /* index 0 is sum num of zero and 1 is also sum num of one */
-unsigned long** tmp_sum_zero_and_one_bit_cnt_arranged_by_err_nums = NULL;
+extern unsigned long** tmp_sum_zero_and_one_bit_cnt_arranged_by_err_nums;
 
 
-double tmp_testMode_loops_zero_bit_ratio = 0.5f;
-unsigned long tmp_testMode_loops_bit_inversion_val = 0UL;
+extern double tmp_testMode_loops_zero_bit_ratio;
+extern unsigned long tmp_testMode_loops_bit_inversion_val;
 
-unsigned long tmp_testMode_loops_cnt = 0UL;
+extern unsigned long tmp_testMode_loops_cnt;
 
-unsigned long tmp_testMode_totalBit = 0UL;
+extern unsigned long tmp_testMode_totalBit;
 
 #define LLR_CASE_NUM    8
 
-unsigned long tmp_testMode_count_bit_w = 0UL;
+extern unsigned long tmp_testMode_count_bit_w;
 
-unsigned long tmp_testMode_llr_mag_w[LLR_CASE_NUM] = {0UL};
+extern unsigned long tmp_testMode_llr_mag_w[LLR_CASE_NUM];
 
-unsigned long tmp_testMode_llr_mag_sum_w = 0UL;
+extern unsigned long tmp_testMode_llr_mag_sum_w;
 
-unsigned int tmp_testMode_llr_mag_max_w = 0;
-unsigned int tmp_testMode_llr_mag_min_w = 7;
+extern unsigned int tmp_testMode_llr_mag_max_w;
+extern unsigned int tmp_testMode_llr_mag_min_w;
 
-double tmp_testMode_llr_mag_avr_w = 0.0f;
+extern double tmp_testMode_llr_mag_avr_w;
 
 
-unsigned long tmp_testMode_count_bit_c = 0UL;
+extern unsigned long tmp_testMode_count_bit_c;
 
-unsigned long tmp_testMode_llr_mag_c[LLR_CASE_NUM] = {0UL};
+extern unsigned long tmp_testMode_llr_mag_c[LLR_CASE_NUM];
 
-unsigned long tmp_testMode_llr_mag_sum_c = 0UL;
+extern unsigned long tmp_testMode_llr_mag_sum_c;
 
-unsigned int tmp_testMode_llr_mag_max_c = 0;
-unsigned int tmp_testMode_llr_mag_min_c = 7;
+extern unsigned int tmp_testMode_llr_mag_max_c;
+extern unsigned int tmp_testMode_llr_mag_min_c;
 
-double tmp_testMode_llr_mag_avr_c = 0.0f;
+extern double tmp_testMode_llr_mag_avr_c;
 
 
 /****************************************************************/
 /***** Probability of Min val *****/
-unsigned int tmp_testMode_prob_distribution_group_nums = 1;
+extern unsigned int tmp_testMode_prob_distribution_group_nums;
 
-unsigned int tmp_testMode_prob_min_counting_length = 0;
-unsigned int *tmp_testMode_prob_min_counting = NULL;
+extern unsigned int tmp_testMode_prob_min_counting_length;
+extern unsigned int *tmp_testMode_prob_min_counting;
 
 enum ENUM_CASE_TESTMODE_PROB_MIN_CHOOSEN_POLITICS
 {
     ENUM_CASE_TEST_MODE_PROB_MIN_CHOOSE_SAME_VAL,
     ENUM_CASE_TEST_MODE_PROB_MIN_CHOOSE_LOW_VAL,
 };
-unsigned int tmp_testMode_prob_min_choosen_politics =
-                ENUM_CASE_TEST_MODE_PROB_MIN_CHOOSE_SAME_VAL;
+extern unsigned int tmp_testMode_prob_min_choosen_politics;
 
 /* Functions */
 unsigned int* initTestMode_prob_min_counting(unsigned int **p, unsigned int *len, unsigned int max_val);
 char clearTestMode_prob_min_counting(unsigned int *p, unsigned int len, unsigned int max_val);
 /* Simulation funtion Test(verification) Mode end */
-
 #endif
